5f83cdf6c93a07c2a5ac39e7626d9e51a13b954f
Steven Haryanto (on HP MiniNote) <stevenharyanto@gmail.com>
2011-07-10 14:20:28 +0700

    previous alias attempt (use parent) failed

--- lib/Calendar/ID/Holiday.pm
+++ lib/Calendar/ID/Holiday.pm
@@ -1,5 +1,9 @@
 package Calendar::ID::Holiday;
-use parent qw(Calendar::Indonesia::Holiday);
+use Calendar::Indonesia::Holiday;
 our %SPEC = %Calendar::Indonesia::Holiday::SPEC;
+for my $f (keys %SPEC) {
+    *{$f} = \&{"Calendar::Indonesia::Holiday::$f"};
+}
+
 1;
 # ABSTRACT: Alias for Calendar::Indonesia::Holiday
