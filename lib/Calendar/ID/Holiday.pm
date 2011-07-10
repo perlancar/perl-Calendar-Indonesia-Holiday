459eec056724fcb84e7b9b4302d029be738fb4c1
Steven Haryanto (on HP MiniNote) <stevenharyanto@gmail.com>
2011-07-10 14:12:56 +0700

    typo

--- lib/Calendar/ID/Holiday.pm
+++ lib/Calendar/ID/Holiday.pm
@@ -1,5 +1,5 @@
 package Calendar::ID::Holiday;
 use parent qw(Calendar::Indonesia::Holiday);
-our %SPEC = Calendar::Indonesia::Holiday::SPEC;
+our %SPEC = %Calendar::Indonesia::Holiday::SPEC;
 1;
 # ABSTRACT: Alias for Calendar::Indonesia::Holiday
